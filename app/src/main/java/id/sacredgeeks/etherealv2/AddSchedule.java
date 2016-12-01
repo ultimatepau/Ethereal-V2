683f62ed4a1b65aeb2447bcc721df885d0f067e3
ultimatepau <mrizaldi2@gmail.com>
2016-12-01 18:24:19 +0700

    Perbaikan Bug dan Penambahan Capture

--- app/src/main/java/id/sacredgeeks/etherealv2/AddSchedule.java
+++ app/src/main/java/id/sacredgeeks/etherealv2/AddSchedule.java
@@ -1,13 +1,17 @@
 package id.sacredgeeks.etherealv2;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -15,32 +19,45 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class AddSchedule extends AppCompatActivity {
 
     private TextView tvTanggal;
     private String tanggal;
+    String imagePath;
+    File destination;
     private int TAKE_PHOTO_CODE = 0;
     public static int count = 0;
+    private static final int REQUEST_IMAGE = 100;
 
     private EditText inputTugas;
     private EditText inputDeadline;
     private EditText inputketTugas;
-    private Calendar myCalendar;
-    /* private int PICK_IMAGE_REQUEST = 1; */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPref = getSharedPreferences("count", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("count",""+count);
+        editor.commit();
+
+        SharedPreferences settings = getSharedPreferences("count", Context.MODE_PRIVATE);
+        String angka = settings.getString("count",null);
+
+        Toast.makeText(getApplicationContext(), "count = "+angka, Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
 
         inputDeadline = (EditText) findViewById(R.id.inputdeadline);
         inputTugas = (EditText) findViewById(R.id.inputnamatugas);
         inputketTugas = (EditText) findViewById(R.id.inputkettugas);
-        inputDeadline.setInputType(0);
 
         tvTanggal = (TextView) findViewById(R.id.tvtanggal);
 
@@ -49,80 +66,134 @@ public class AddSchedule extends AppCompatActivity {
 
         tvTanggal.setText("Tanggal : "+tanggal);
 
-        myCalendar = Calendar.getInstance();
-        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
-            }
-
-        };
-        inputDeadline.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new DatePickerDialog(AddSchedule.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
     }
 
     public void browseImg(View view) {
-        /*
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-        */
-        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/imgTugas/";
-        File newdir = new File(dir);
-        newdir.mkdirs();
+        File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+File.separator+ "imgTugas");
+        if (!folder.exists()) {
+            folder.mkdirs();
+        } else {
 
-        count++;
-        String file = dir+count+".jpg";
-        File newfile = new File(file);
-        String nomedia = dir+".nomedia";
-        File createnomedia = new File(nomedia);
-        try {
-            newfile.createNewFile();
-            createnomedia.createNewFile();
-        }
-        catch (IOException e)
-        {
         }
 
-        Uri outputFileUri = Uri.fromFile(newfile);
+        SharedPreferences settings = getSharedPreferences("count", Context.MODE_PRIVATE);
+        String angka = settings.getString("count",null);
 
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        String name = "Foto"+angka+".jpg";
+        destination = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "/imgTugas/"+name);
 
-        startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
-
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(destination));
+        startActivityForResult(intent, REQUEST_IMAGE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        if( requestCode == REQUEST_IMAGE && resultCode == Activity.RESULT_OK ){
+            try {
+                FileInputStream in = new FileInputStream(destination);
+                //BitmapFactory.Options options = new BitmapFactory.Options();
+                //options.inSampleSize = 10;
+                imagePath = destination.getAbsolutePath();
 
-        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
-            Toast.makeText(getApplicationContext(), "Foto Tersimpan !", Toast.LENGTH_SHORT).show();
-            Log.d("CameraDemo", "Pic saved");
+                SharedPreferences settings = getSharedPreferences("count", Context.MODE_PRIVATE);
+                String angka = settings.getString("count",null);
+                Integer lol = Integer.parseInt(angka);
+                lol++;
+
+                SharedPreferences sharedPref = getSharedPreferences("count", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("count",""+lol);
+                editor.apply();
+                //Bitmap bmp = BitmapFactory.decodeStream(in, null, options);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Request Cancelled",Toast.LENGTH_SHORT).show();
         }
     }
 
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    public String dateToString(Date date, String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        return df.format(date);
+    }
+
+    public void inputDate(View view) {
+        new DatePickerDialog(AddSchedule.this, date, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
 
     private void updateLabel() {
-        myCalendar = Calendar.getInstance();
 
         String myFormat = "dd MMM yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
         inputDeadline.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    public void submit(View view) {
+        message(AddSchedule.this,"Nama : "+inputTugas.getText());
+
+        new CountDownTimer(1500, 1000) {
+            public void onFinish() {
+                // When timer is finished
+                // Execute your code here
+                message(AddSchedule.this,"Keterangan : "+inputketTugas.getText());
+            }
+
+            public void onTick(long millisUntilFinished) {
+                // millisUntilFinished    The amount of time until finished.
+            }
+        }.start();
+
+        new CountDownTimer(2500, 1000) {
+            public void onFinish() {
+                // When timer is finished
+                // Execute your code here
+                message(AddSchedule.this,"Deadline : "+inputDeadline.getText());
+            }
+
+            public void onTick(long millisUntilFinished) {
+                // millisUntilFinished    The amount of time until finished.
+            }
+        }.start();
+
+        new CountDownTimer(4500, 1000) {
+            public void onFinish() {
+                // When timer is finished
+                // Execute your code here
+                message(AddSchedule.this,"ImagePath : "+imagePath);
+            }
+
+            public void onTick(long millisUntilFinished) {
+                // millisUntilFinished    The amount of time until finished.
+            }
+        }.start();
+
+    }
+
+    public  static void message(Activity activity,String message) {
+        View rootView = activity.getWindow().getDecorView().findViewById(android.R.id.content);
+        Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT).show();
     }
 }
